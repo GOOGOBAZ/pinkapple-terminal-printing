6ae52bc990041b1c9841005f57a57cb2b9b0a552
GOOGOBAZ <augbazi@gmail.com>
2025-04-15 11:09:11 +0300

    Change company details endpoint to order results by company_detail_id in ascending order

--- app.js
+++ app.js
@@ -3463,7 +3463,7 @@ app.get('/company-details', async (req, res) => {
          created_at,
          updated_at
        FROM company_details
-       ORDER BY company_detail_id DESC`
+       ORDER BY company_detail_id ASC`
     );
     return res.status(200).json({ data: rows });
   } catch (err) {
